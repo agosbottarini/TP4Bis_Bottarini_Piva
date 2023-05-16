42604d02c33eee11922f8e84eaa21dfa17f474d6
JOAQUIN PIVA <47870976@ort>
2023-05-16 10:27:52 -0300

    crando info

---
+++ Models/Info.cs
@@ -0,0 +1,37 @@
+public static class Info
+{
+    static List <Pais> ListaPaises = new List<Pais>();
+
+    public static void  IniciarLista()
+    {
+        
+        ListaPaises.Add(new Pais("Argentina", imgBandera, poblacion, fechaIndependencia, atractivosTuristicos)) ;
+        ListaPaises.Add(new Pais(Brasil, imgBandera, poblacion, fechaIndependencia, atractivosTuristicos));
+        ListaPaises.Add(new Pais(nombre, imgBandera, poblacion, fechaIndependencia, atractivosTuristicos));
+        ListaPaises.Add(new Pais(nombre, imgBandera, poblacion, fechaIndependencia, atractivosTuristicos));
+        ListaPaises.Add(new Pais(nombre, imgBandera, poblacion, fechaIndependencia, atractivosTuristicos));
+    }
+
+    public static List<Pais> ListarPaises()
+    {
+        if(ListaPaises.Count == 0)
+        {
+            IniciarLista();
+            return ListaPaises;
+        }
+        return ListaPaises; 
+    }
+
+    public static Pais DetallePais(string Nombre)
+    {
+        foreach(Pais busqueda in ListaPaises)
+        {
+            if(busqueda.Nombre == Nombre)
+            {
+                return busqueda;
+            }
+        }
+        return null;
+        
+    }
+}
